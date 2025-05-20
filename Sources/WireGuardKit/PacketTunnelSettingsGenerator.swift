8ba88ae7cc3ce6d6c70e0b848a2ecb766bf9d0b5
Bethurel Griffin <bgriffin@BGRIFFIN-CON-MBA.local>
2025-05-20 15:33:15 -0400

    NEW-5295: Add Bypass IP Addresses to excludedRoutes in Network Extension

--- Sources/WireGuardKit/PacketTunnelSettingsGenerator.swift
+++ Sources/WireGuardKit/PacketTunnelSettingsGenerator.swift
@@ -118,6 +118,23 @@ class PacketTunnelSettingsGenerator {
 
         let ipv4Settings = NEIPv4Settings(addresses: ipv4Addresses.map { $0.destinationAddress }, subnetMasks: ipv4Addresses.map { $0.destinationSubnetMask })
         ipv4Settings.includedRoutes = ipv4IncludedRoutes
+        
+        // TODO: - Implement API to provide excluded routes once endpoint is available.
+        let excludedRouteStrings = ["104.19.222.79", "104.19.223.79"]
+
+        var excludedRoutes = [NEIPv4Route]()
+
+        for exludedRouteString in excludedRouteStrings {
+            if let exclRange = IPAddressRange(from: exludedRouteString) {
+                let route = NEIPv4Route(destinationAddress: "\(exclRange.maskedAddress())", subnetMask: "\(exclRange.subnetMask())")
+                excludedRoutes.append(NEIPv4Route(destinationAddress: route.destinationAddress, subnetMask: route.destinationSubnetMask))
+            }
+        }
+
+        if !excludedRoutes.isEmpty {
+            ipv4Settings.excludedRoutes = excludedRoutes
+        }
+        
         networkSettings.ipv4Settings = ipv4Settings
 
         let ipv6Settings = NEIPv6Settings(addresses: ipv6Addresses.map { $0.destinationAddress }, networkPrefixLengths: ipv6Addresses.map { $0.destinationNetworkPrefixLength })
